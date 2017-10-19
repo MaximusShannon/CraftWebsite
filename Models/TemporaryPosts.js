acac0fe8dfb703bc7dab053a17a7afc7df309fe1
Max Shannon <Maxshannon2016@hotmail.com>
2017-10-19 19:56:23 +0100

    Initial Posts Structure

---
+++ Models/TemporaryPosts.js
@@ -0,0 +1,91 @@
+var posts = [
+
+    //************************* Cards/Paperwork *******************
+    {
+        title: "Edels Cards",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Cards for all occasions",
+        price: 7.99,
+        tags: "Cards, Birthday, Birthdays, Christmas, deaths, births, wedding",
+        date: Date.now(),
+        postId: 1000
+    },
+    {
+        title: "Trinas Cards",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Cards for special Occasions",
+        price: 15.99,
+        tags: "Christmas, Birthdays, wedding",
+        date: Date.now(),
+        postId: 1001
+    },
+    {
+        title: "SadMan Cards",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Cards for most bad occasions",
+        price: 10.99,
+        tags: "deaths, births, divorce",
+        date: Date.now(),
+        postId: 1002
+    },
+    {
+
+    },
+    //************************* Flowers *******************
+    {
+        title: "Damons Flowers",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Flowers made for you",
+        price: 9.99,
+        tags: "deaths, birthdays, valentines, roses, red, yellow",
+        date: Date.now(),
+        postId: 1003
+    },
+    {
+        title: "Daynas Colourful Creations Flowers",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Flowers made for all occasions",
+        price: 19.99,
+        tags: "deaths, birthdays, valentines, births, divorces",
+        date: Date.now(),
+        postId: 1004
+    },
+    {
+        title: "Veroincas Flowers for deaths",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Flowers for deaths",
+        price: 25.99,
+        tags: "deaths",
+        date: Date.now(),
+        postId: 1005
+    },
+    //************************* Cloths Work *******************
+    {
+        title: "Daynas Knitting",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Specialised knitting for all reasons",
+        price: 17.99,
+        tags: "Jumpers, t-shirts, pants, babies cloths, jeans, pants",
+        date: Date.now(),
+        postId: 1006
+    },
+    {
+        title: "Donas Animal Knitting",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "Specialised knitting for all reasons",
+        price: 17.99,
+        tags: "animals, cloths, pants, jeans, jumpers",
+        date: Date.now(),
+        postId: 1007
+    },
+    {
+        title: "Michaels Boys Cloths Knitting",
+        imageReferences: [{img1: "xxx",img2: "xxx",img3: "xxx"}],
+        infoTitle: "For boys school cloths",
+        price: 49.99,
+        tags: "school jumpers, school t-shirts, school pants",
+        date: Date.now(),
+        postId: 1008
+    }
+
+];
