469cb19535c969ae9ee758b9044a631924315e2f
Max Shannon <Maxshannon2016@hotmail.com>
2017-10-19 16:10:18 +0100

    findAll and findOne functions added

--- routes/User.js
+++ routes/User.js
@@ -9,3 +9,15 @@ function getByValue(arr, id){
     return result ? result[0] : null;
 }
 
+router.findAll = function (req, res) {
+    res.json(users);
+}
+
+router.findOne = function (req, res) {
+    var user = getByValue(users, req.params.id);
+
+    if(user != null)
+        res.json(user);
+    else
+        res.json({message: "Profile Not Found"});
+};
