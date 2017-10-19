d562051d20ff5b49042bc07eae0fe23b218e8de7
Max Shannon <Maxshannon2016@hotmail.com>
2017-10-19 16:55:26 +0100

    findAll and findOne endpoints added

--- Models/TemporaryUsers.js
+++ Models/TemporaryUsers.js
@@ -1,6 +1,6 @@
 var users = [
-    {userName: "MaxShannon", email: "maxshannon2016@hotmail.com", password: "howiye123", bio: "hup,derp,hup", id: "1000", profilePictureRef:""},
-    {userName: "DaynaEarls", email: "daynaearls@hotmail.com", password: "testtest123", bio: "derpy, herpy, lerpy", id: "1001", profilePictureRef:""}
+    {userName: "MaxShannon", email: "maxshannon2016@hotmail.com", password: "howiye123", bio: "hup,derp,hup", id: 1000, profilePictureRef:""},
+    {userName: "DaynaEarls", email: "daynaearls@hotmail.com", password: "testtest123", bio: "derpy, herpy, lerpy", id: 1001, profilePictureRef:""}
 ];
 
-moduel.exports = users;
+module.exports = users;
--- routes/User.js
+++ routes/User.js
@@ -5,13 +5,16 @@ var router = express.Router();
 //********************** Support Functions **********************
 
 function getByValue(arr, id){
-    var result = arr.filter(function (t) { t.id == id; })
+    var result = arr.filter(function(t){return t.id == id;})
+
     return result ? result[0] : null;
 }
 
+//********************** Router Functions **********************
+
 router.findAll = function (req, res) {
     res.json(users);
-}
+};
 
 router.findOne = function (req, res) {
     var user = getByValue(users, req.params.id);
@@ -19,5 +22,7 @@ router.findOne = function (req, res) {
     if(user != null)
         res.json(user);
     else
-        res.json({message: "Profile Not Found"});
+        res.json({message: "Couldn't find user"});
 };
+
+module.exports = router;
