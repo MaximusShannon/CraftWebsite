6afed03eb5e9daa675c3d24fb9c0e9e3c75674ce
Max Shannon <Maxshannon2016@hotmail.com>
2017-11-12 19:31:48 +0000

    Linting complete

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var index = require('./routes/index');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var index = require("./routes/index");
 
-var user = require('./routes/User');
-var posts = require('./routes/Posts');
+var user = require("./routes/User");
+var posts = require("./routes/Posts");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,48 +21,48 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //app.use('/', index);
 
 //User endpoints
-app.get('/TemporaryUsers', user.findAll);
-app.get('/TemporaryUsers/:id', user.findOne);
-app.post('/TemporaryUsers', user.addUser);
-app.put('/TemporaryUsers/:id/bio', user.updateProfileBio);
-app.delete('/TemporaryUsers/:id', user.deleteUser);
+app.get("/TemporaryUsers", user.findAll);
+app.get("/TemporaryUsers/:id", user.findOne);
+app.post("/TemporaryUsers", user.addUser);
+app.put("/TemporaryUsers/:id/bio", user.updateProfileBio);
+app.delete("/TemporaryUsers/:id", user.deleteUser);
 
 //Posts endpoints
-app.get('/TemporaryPosts', posts.findAllPosts);
-app.get('/TemporaryPosts/:postId', posts.findOne);
-app.get('/TemporaryPostsByTags/:tags', posts.findAllPostsByTag);
-app.get('/TemporaryPostsByLowerPrice/:price', posts.findAllLessThanPrice);
+app.get("/TemporaryPosts", posts.findAllPosts);
+app.get("/TemporaryPosts/:postId", posts.findOne);
+app.get("/TemporaryPostsByTags/:tags", posts.findAllPostsByTag);
+app.get("/TemporaryPostsByLowerPrice/:price", posts.findAllLessThanPrice);
 
 
 //come back to this
 //save for extra marks maybe? ;)
 //app.get('TemporaryPostsCategory/:id/category', posts.findCategoryFuzzySearch);
 
-if(process.env.NODE_ENV != 'test'){
-    app.use(logger('dev'));
+if(process.env.NODE_ENV != "test"){
+    app.use(logger("dev"));
 }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
