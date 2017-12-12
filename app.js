f9dce6650b49d944cd6df3b2e47accfc8135bf52
Max Shannon <Maxshannon2016@hotmail.com>
2017-12-12 19:11:22 +0000

    Production build failed

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
-
 var routes = require('./routes/index');
 var user = require('./routes/User');
 var posts = require('./routes/Posts');
